e581465b79538e4a34b75b3666e5e85e06b34a37
Ansuman Pratap Chauhan <ansuman.it@gmail.com>
2022-06-16 15:23:31 +0530

    Perfect Square Php
    Perfect Square -Php

--- perfect_square.php
+++ perfect_square.php
@@ -1,34 +1,33 @@
 <?php
 
+/*      Perfect Square Count    */
+
 function isPerfectSquare($x){
+    $c = 0;
+    foreach ($x as $value){
 
-	$c=0;
+        $sqroot = sqrt($value);
+        $floor = floor($sqroot);
 
-	foreach ($x as  $value) {
+        if ($sqroot - $floor == 0){
+            $c = $c + 1;
+        }
 
+    }
 
-		$sqroot=sqrt($value);
-
-		$floor= floor($sqroot);
-
-		if($sqroot - $floor == 0){
-
-			$c=$c+1;
-		}
-		
-	}
-
-	
-	return $c;
+    return $c;
 }
 
-
-
-$n = array('10','81','33','55','64');
+$n = array(
+    '10',
+    '81',
+    '33',
+    '55',
+    '64'
+);
 
 $count = isPerfectSquare($n);
 
-echo 'Perfect Square Count==>'.$count ;
-
+echo 'Perfect Square Count==>'. $count;
 
 ?>
